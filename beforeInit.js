394889ee69ed61358ae0266be02a73332db3a6c1
Jeverson Miotti <jeverson.miotti@saveincloud.com>
2023-10-06 16:42:22 -0300

    Validação IP Público condicional

--- beforeInit.js
+++ beforeInit.js
@@ -4,6 +4,32 @@ var resp = {
 }
 
 if ('${settings.storagebackup:false}'== 'true') {
+  if ('${settings.ippublic:false}'== 'true') {
+  resp.nodes.push({
+    nodeType: "storage-backup",
+    nodeGroup: "storage",    
+    fixedCloudlets: 1,
+    cloudlets: 4,
+    diskLimit: 20
+  }, {
+  nodeType: "ubuntu-vps",
+  tag: ${settings.ubuntu_version},  
+  fixedCloudlets: 1,
+  cloudlets: 4,
+  nodeGroup: "cp",
+  extip: true,
+  isSLBAccessEnabled: false,
+    env: {
+      JELASTIC_PORTS: "3050, 21"      
+    },      
+    volumes: [
+      "/opt/firebird",
+      "/opt/firebird/logs",
+      "/opt/firebird/data",
+      "/backup/"
+    ]
+  })
+} else {
   resp.nodes.push({
     nodeType: "storage-backup",
     nodeGroup: "storage",    
@@ -26,23 +52,24 @@ if ('${settings.storagebackup:false}'== 'true') {
       "/backup/"
     ]
   })
-} else {
-  resp.nodes.push({
-  nodeType: "ubuntu-vps",
-  tag: ${settings.ubuntu_version},  
-  fixedCloudlets: 1,
-  cloudlets: 4,
-  nodeGroup: "cp",
-    env: {
-      JELASTIC_PORTS: "3050, 21"      
-    },      
-    volumes: [
-      "/opt/firebird",
-      "/opt/firebird/logs",
-      "/opt/firebird/data",
-      "/backup/"
-    ]
-  })
+}}
+else {
+    resp.nodes.push({
+    nodeType: "ubuntu-vps",
+    tag: ${settings.ubuntu_version},  
+    fixedCloudlets: 1,
+    cloudlets: 4,
+    nodeGroup: "cp",
+      env: {
+        JELASTIC_PORTS: "3050, 21"      
+      },      
+      volumes: [
+        "/opt/firebird",
+        "/opt/firebird/logs",
+        "/opt/firebird/data",
+        "/backup/"
+      ]
+    })
 }
   
 return resp;
