0ef5c9360f8d4361ffe31d7499bbfe3186021b05
Jeverson Miotti <jeverson.miotti@saveincloud.com>
2023-10-06 16:36:21 -0300

    Final-ok

--- beforeInit.js
+++ beforeInit.js
@@ -11,88 +11,38 @@ if ('${settings.storagebackup:false}'== 'true') {
     cloudlets: 4,
     diskLimit: 20
   }, {
-    if ('${settings.ippublic:false}'== 'true') {
-
-      resp.nodes.push({
-        nodeType: "ubuntu-vps",
-        tag: ${settings.ubuntu_version},  
-        fixedCloudlets: 1,
-        cloudlets: 4,
-        nodeGroup: "cp",
-        extip: true,
-        isSLBAccessEnabled: false,
-          env: {
-            JELASTIC_PORTS: "3050, 21"      
-          },      
-          volumes: [
-            "/opt/firebird",
-            "/opt/firebird/logs",
-            "/opt/firebird/data",
-            "/backup/"
-          ]
-        })
-  
-    }
-    else {
-      resp.nodes.push({
-      nodeType: "ubuntu-vps",
-      tag: ${settings.ubuntu_version},  
-      fixedCloudlets: 1,
-      cloudlets: 4,
-      nodeGroup: "cp",
-        env: {
-          JELASTIC_PORTS: "3050, 21"      
-        },      
-        volumes: [
-          "/opt/firebird",
-          "/opt/firebird/logs",
-          "/opt/firebird/data",
-          "/backup/"
-        ]
-      }
-  }
+  nodeType: "ubuntu-vps",
+  tag: ${settings.ubuntu_version},  
+  fixedCloudlets: 1,
+  cloudlets: 4,
+  nodeGroup: "cp",
+    env: {
+      JELASTIC_PORTS: "3050, 21"      
+    },      
+    volumes: [
+      "/opt/firebird",
+      "/opt/firebird/logs",
+      "/opt/firebird/data",
+      "/backup/"
+    ]
   })
 } else {
-  if ('${settings.ippublic:false}'== 'true') {
-
-    resp.nodes.push({
-      nodeType: "ubuntu-vps",
-      tag: ${settings.ubuntu_version},  
-      fixedCloudlets: 1,
-      cloudlets: 4,
-      nodeGroup: "cp",
-      extip: true,
-      isSLBAccessEnabled: false,
-        env: {
-          JELASTIC_PORTS: "3050, 21"      
-        },      
-        volumes: [
-          "/opt/firebird",
-          "/opt/firebird/logs",
-          "/opt/firebird/data",
-          "/backup/"
-        ]
-      })
-
-  }
-  else {
-    resp.nodes.push({
-    nodeType: "ubuntu-vps",
-    tag: ${settings.ubuntu_version},  
-    fixedCloudlets: 1,
-    cloudlets: 4,
-    nodeGroup: "cp",
-      env: {
-        JELASTIC_PORTS: "3050, 21"      
-      },      
-      volumes: [
-        "/opt/firebird",
-        "/opt/firebird/logs",
-        "/opt/firebird/data",
-        "/backup/"
-      ]
-    }
+  resp.nodes.push({
+  nodeType: "ubuntu-vps",
+  tag: ${settings.ubuntu_version},  
+  fixedCloudlets: 1,
+  cloudlets: 4,
+  nodeGroup: "cp",
+    env: {
+      JELASTIC_PORTS: "3050, 21"      
+    },      
+    volumes: [
+      "/opt/firebird",
+      "/opt/firebird/logs",
+      "/opt/firebird/data",
+      "/backup/"
+    ]
+  })
 }
   
-} 
 return resp;
