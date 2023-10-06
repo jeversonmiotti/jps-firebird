20091cf3eda93f99d0613c0fb1760bcd1b4de4e6
Jeverson Miotti <jeverson.miotti@saveincloud.com>
2023-10-06 14:10:58 -0300

    Teste1410

--- beforeInit.js
+++ beforeInit.js
@@ -1,35 +1,48 @@
 var resp = {
   result: 0,
   nodes: []
-};
+}
 
-if (${settings.storage_backup:true}) {
+if ('${settings.storagebackup:false}'== 'true') {
   resp.nodes.push({
     nodeType: "storage-backup",
-    nodeGroup: "storage",
-    skipNodeEmails: true,
+    nodeGroup: "storage",    
     fixedCloudlets: 1,
     cloudlets: 4,
     diskLimit: 20
-  });
-}
- 
-  resp.nodes.push({
-    nodeType: "ubuntu-vps",
-  tag: ${settings.ubuntu_version},
-  skipNodeEmails: true,
+  }, {
+  nodeType: "ubuntu-vps",
+  tag: ${settings.ubuntu_version},  
   fixedCloudlets: 1,
   cloudlets: 4,
   nodeGroup: "cp",
-    env: [
-      "JELASTIC_PORTS: 3050, 21"
-    ],
+    env: {
+      JELASTIC_PORTS: "3050, 21"      
+    },      
     volumes: [
-      "/opt/firebird/",
-      "/opt/firebird/logs/",
-      "/opt/firebird/data/",
+      "/opt/firebird",
+      "/opt/firebird/logs",
+      "/opt/firebird/data",
       "/backup/"
     ]
-  });
+  })
+} else {
+  resp.nodes.push({
+  nodeType: "ubuntu-vps",
+  tag: ${settings.ubuntu_version},  
+  fixedCloudlets: 1,
+  cloudlets: 4,
+  nodeGroup: "cp",
+    env: {
+      JELASTIC_PORTS: "3050, 21"      
+    },      
+    volumes: [
+      "/opt/firebird",
+      "/opt/firebird/logs",
+      "/opt/firebird/data",
+      "/backup/"
+    ]
+  })
+}
   
-  return resp;
+return resp;
