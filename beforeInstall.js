1738685c9b7451b1ca3f2e9794ec9b82af023dc5
Jeverson Miotti <jeverson.miotti@saveincloud.com>
2023-10-09 11:24:24 -0300

    Nova versão

---
+++ beforeInstall.js
@@ -0,0 +1,97 @@
+var resp = {
+  result: 0,
+  nodes: []
+}
+
+if ('${settings.storagebackup:false}'== 'true') {
+  if ('${settings.ippublic:false}'== 'true') {
+  resp.nodes.push({
+    nodeType: "storage-backup",
+    nodeGroup: "storage",    
+    fixedCloudlets: 1,
+    cloudlets: 4,
+    diskLimit: 20
+  }, {
+  nodeType: "ubuntu-vps",
+  tag: ${settings.ubuntu_version},  
+  fixedCloudlets: 1,
+  cloudlets: 4,
+  nodeGroup: "cp",
+  extip: true,
+  isSLBAccessEnabled: false,
+    env: {
+      JELASTIC_PORTS: "3050, 21"      
+    },      
+    volumes: [
+      "/opt/firebird",
+      "/opt/firebird/logs",
+      "/opt/firebird/data",
+      "/backup/"
+    ]
+  })
+} else {
+  resp.nodes.push({
+    nodeType: "storage-backup",
+    nodeGroup: "storage",    
+    fixedCloudlets: 1,
+    cloudlets: 4,
+    diskLimit: 20
+  }, {
+  nodeType: "ubuntu-vps",
+  tag: ${settings.ubuntu_version},  
+  fixedCloudlets: 1,
+  cloudlets: 4,
+  nodeGroup: "cp",
+    env: {
+      JELASTIC_PORTS: "3050, 21"      
+    },      
+    volumes: [
+      "/opt/firebird",
+      "/opt/firebird/logs",
+      "/opt/firebird/data",
+      "/backup/"
+    ]
+  })
+}
+}
+else {//no storage
+  if ('${settings.ippublic:false}'== 'true') {
+    resp.nodes.push({
+    nodeType: "ubuntu-vps",
+    tag: ${settings.ubuntu_version},  
+    fixedCloudlets: 1,
+    cloudlets: 4,
+    nodeGroup: "cp",
+    extip: true,
+    isSLBAccessEnabled: false,
+      env: {
+        JELASTIC_PORTS: "3050, 21"      
+      },      
+      volumes: [
+        "/opt/firebird",
+        "/opt/firebird/logs",
+        "/opt/firebird/data",
+        "/backup/"
+      ]
+    })
+  } else {
+    resp.nodes.push({
+    nodeType: "ubuntu-vps",
+    tag: ${settings.ubuntu_version},  
+    fixedCloudlets: 1,
+    cloudlets: 4,
+    nodeGroup: "cp",
+      env: {
+        JELASTIC_PORTS: "3050, 21"      
+      },      
+      volumes: [
+        "/opt/firebird",
+        "/opt/firebird/logs",
+        "/opt/firebird/data",
+        "/backup/"
+      ]
+    })
+  }
+}
+  
+return resp;
