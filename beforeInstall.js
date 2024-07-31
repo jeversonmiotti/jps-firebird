d5cd5568faaa98f8f90cc9920a625ac83ba49693
Petherson <42210646+PethersonML@users.noreply.github.com>
2024-07-31 17:59:42 -0300

    Update beforeInstall.js

--- beforeInstall.js
+++ beforeInstall.js
@@ -3,99 +3,54 @@ var resp = {
   nodes: []
 }
 
-if ('${settings.storagebackup:false}'== 'true') {
-  if ('${settings.ippublic:false}'== 'true') {
+if('${settings.storagebackup:false}'== 'true'){
   resp.nodes.push({
     nodeType: "storage-backup",
     nodeGroup: "storage",    
     fixedCloudlets: 1,
     cloudlets: 4,
-    diskLimit: 20
-  }, {
-  nodeType: "ubuntu-vps",
-  tag: ${settings.ubuntu_version},  
-  fixedCloudlets: 1,
-  cloudlets: 4,
-  nodeGroup: "cp",
-  extip: true,
-  globals: {success: "publicip"},
-  isSLBAccessEnabled: false,
-    env: {
-      JELASTIC_PORTS: "3050, 21"      
-    },      
-    volumes: [
-      "/opt/firebird",
-      "/opt/firebird/logs",
-      "/opt/firebird/data",
-      "/backup"
-    ]
-  })
-} else {
+    iskLimit: 20
+  });
+}
+
+if ('${settings.ippublic:false}'== 'true'){
   resp.nodes.push({
-    nodeType: "storage-backup",
-    nodeGroup: "storage",    
-    fixedCloudlets: 1,
-    cloudlets: 4,
-    diskLimit: 20
-  }, {
-  nodeType: "ubuntu-vps",
-  tag: ${settings.ubuntu_version},  
-  fixedCloudlets: 1,
-  cloudlets: 4,
-  globals: {success: "nopublicip"},
-  nodeGroup: "cp",
-    env: {
-      JELASTIC_PORTS: "3050, 21"      
-    },      
-    volumes: [
-      "/opt/firebird",
-      "/opt/firebird/logs",
-      "/opt/firebird/data",
-      "/backup"
-    ]
-  })
-}
-}
-else {//no storage
-  if ('${settings.ippublic:false}'== 'true') {
-    resp.nodes.push({
     nodeType: "ubuntu-vps",
+    nodeGroup: "cp",
     tag: ${settings.ubuntu_version},  
     fixedCloudlets: 1,
     cloudlets: 4,
-    nodeGroup: "cp",
+    isSLBAccessEnabled: false,
     extip: true,
     globals: {success: "publicip"},
-    isSLBAccessEnabled: false,
-      env: {
-        JELASTIC_PORTS: "3050, 21"      
-      },      
-      volumes: [
-        "/opt/firebird",
-        "/opt/firebird/logs",
-        "/opt/firebird/data",
-        "/backup"
-      ]
-    })
-  } else {
-    resp.nodes.push({
+    env: {
+      JELASTIC_PORTS: "3050, 21"      
+    },      
+    volumes: [
+      "/opt/firebird",
+      "/opt/firebird/logs",
+      "/opt/firebird/data",
+      "/backup"
+    ]
+  });
+} else {
+  resp.nodes.push({
     nodeType: "ubuntu-vps",
+    nodeGroup: "cp",
     tag: ${settings.ubuntu_version},  
     fixedCloudlets: 1,
     cloudlets: 4,
     globals: {success: "nopublicip"},
-    nodeGroup: "cp",
-      env: {
-        JELASTIC_PORTS: "3050, 21"      
-      },      
-      volumes: [
-        "/opt/firebird",
-        "/opt/firebird/logs",
-        "/opt/firebird/data",
-        "/backup"
-      ]
-    })
-  }
+    env: {
+      JELASTIC_PORTS: "3050, 21"      
+    },      
+    volumes: [
+      "/opt/firebird",
+      "/opt/firebird/logs",
+      "/opt/firebird/data",
+      "/backup"
+    ]
+  });
 }
 
 return resp;
